c88691fe77d85b6873053ddbb607046e4903bf56
mengxue.wei <1834093885@qq.com>
2018-07-25 16:34:20 +0800

    Initial Commit

---
+++ XXCycleProcessAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXCycleProcessAnimation
+//
+//  Created by 人人 on 2018/7/25.
+//  Copyright © 2018年 shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XXCycleProcessAnimation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XXCycleProcessAnimation
+//
+//  Created by 人人 on 2018/7/25.
+//  Copyright © 2018年 shine. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
